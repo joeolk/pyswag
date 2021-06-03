e7beaf50fe2f1919235ab5d1914cea9703692d14
joe <joeolk@gmail.com>
2021-06-03 09:03:54 -0400

    updating, WIP

--- src/code/resume/get_resume.py
+++ src/code/resume/get_resume.py
@@ -9,8 +9,6 @@ def get_resume():
     """
     s3 = client(
         's3',
-        aws_access_key_id='AKIAVTMRRLGEXEKR32XL',
-        aws_secret_access_key='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'
     )
 
     file = s3.get_object(Bucket='pyswag-joe', Key='JosephOlkowitzResume.pdf')
